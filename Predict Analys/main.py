66eb5050721a1e72709e7a53d4e73594f986bfa4
adimemes <adimemes06@gmail.com>
2025-03-26 00:10:39 +0800

    First Commit

--- Predict Analys/main.py
+++
@@ -1,9 +0,0 @@
-import pandas as pd
-import numpy as np  
-import matplotlib.pyplot as plt
-import seaborn as sns
-
-
-url = 'https://github.com/tidyverse/ggplot2/tree/main/data-raw'
-diamond = pd.read_csv(url)
-print(diamond.head)
